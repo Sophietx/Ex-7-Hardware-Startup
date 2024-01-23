d1938c95a0ce7dcea3d7da32df261024a7449681
Sophietx <sophiesaleh@dpengineering.org>
2024-01-23 11:04:08 -0800

    updating motorgui so my stupid code works

---
+++ MotorGUI/mainmotor.py
@@ -0,0 +1,61 @@
+
+#os.environ['DISPLAY'] = ":0.0"
+#os.environ['KIVY_WINDOW'] = 'egl_rpi'
+import pygame
+import os
+
+from pidev.Joystick import Joystick
+
+from kivy.clock import Clock
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty
+from kivy.uix.slider import Slider
+
+from pidev.MixPanel import MixPanel
+from pidev.kivy.PassCodeScreen import PassCodeScreen
+from pidev.kivy.PauseScreen import PauseScreen
+from pidev.kivy import DPEAButton
+from pidev.kivy import ImageButton
+from pidev.kivy.selfupdatinglabel import SelfUpdatingLabel
+from kivy.uix.image import Image
+from kivy.animation import Animation
+
+from datetime import datetime
+
+
+
+time = datetime
+
+MIXPANEL_TOKEN = "x"
+MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
+
+SCREEN_MANAGER = ScreenManager()
+FIRST_SCREEN_NAME = 'first'
+
+class ProjectNameGUI(App):
+    """
+    Class to handle running the GUI Application
+    """
+
+    def build(self):
+        """
+        Build the application
+        :return: Kivy Screen Manager instance
+        """
+        return SCREEN_MANAGER
+
+
+Window.clearcolor = (.132, .156, .194, 1)  # White
+
+button_toggle = False
+
+class FirstScreen(Screen):
+    pass
+
+Builder.load_file('first.kv')
+SCREEN_MANAGER.add_widget(FirstScreen(name=FIRST_SCREEN_NAME))
